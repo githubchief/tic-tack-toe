36bd896a0c0c691bacd70fd0bf67377447f97d3f
githubchief <ranganathanshams@gmail.com>
2022-12-31 21:50:00 +0530

    implement checkForWin function in js

--- main.js
+++ main.js
@@ -13,33 +13,45 @@ const player = (sign) => {
 const gameBoard = (
     ()=> {
 
-//arr to store the player markings
+        //arr to store the player markings
         let gameArr = ["", "", "",
                         "", "", "",
                         "", "", ""];
         
-//create two players for each sign
+        //create two players for each sign
         let playerX = player("x");
         let playerO = player("o");
 
-//selecting all 9 cells in the game board
+        //
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        //selecting all 9 cells in the game board
         let cells = document.querySelectorAll(".cell");
 
-//variable to check if game is over or not after each update
+        //variable to check if game is over or not after each update
         let isGameOver = false;
 
-//variable to track the number of marking/update in the game board
+        //variable to track the number of marking/update in the game board
         let updateCount = 1;
 
-//add event listeners for each cell
+        //add event listeners for each cell
         cells.forEach((cell, id) => {
             cell.addEventListener(("click"), () => {
                 
-//when a cell is clicked gets id using getAtrribute
+                //when a cell is clicked get its id using getAtrribute
                 id = cell.getAttribute("id");
                 id=id.charAt(id.length-1);
 
-//use id as array index for the gameArr, and update signs alternatively beginning with X sign
+                //use id as array index for the gameArr, and update signs alternatively beginning with X sign
                 if (updateCount%2 == 1) {
                     updateGameBoard(id, playerX.getSign());
                 }      
@@ -50,7 +62,7 @@ const gameBoard = (
             });
         });
 
-//function to draw the game board
+        //function to draw the game board
         const drawGameBoard = () => {
             cells.forEach((cell, id) => {
                 id = cell.getAttribute("id");
@@ -59,42 +71,51 @@ const gameBoard = (
             });
         };
 
-//function to update game for each markings
+        //function to update game for each markings
         const updateGameBoard = (id, mark) => {
 
             //if a cell is empty and if game is not over, only then update the cell.
             if ((gameArr[id-1] == "") && (!isGameOver)) {
+
                 gameArr[id-1] = mark;
-            
-            //draw the game board again to update the changes in UI
+                
+                //draw the game board again to update the changes in UI
                 drawGameBoard();
                 updateCount++;
             
-            //after each update in the gameArr check if someone has won the game or not
-            gameStatus();
+                //after each update in the gameArr check if someone has won the game or not
+                checkForWin("x");
+                checkForWin("o");
             }
         };
 
-//function to check if game is over or not
-        const gameStatus = () => {
-
-            if( (gameArr[9] == "X" ) || (gameArr[9] == "O" ) )
-            {
-                    isGameOver = true; 
+        //function to check if game is over or not
+        function checkForWin(player) {
+            // Loop through each winning combination
+            for (let i = 0; i < winningCombinations.length; i++) {
+              // Get the current winning combination
+              const combination = winningCombinations[i];
+              // Check if the player has all the positions in the combination
+              const hasWon = combination.every(index => gameArr[index] === player);
+              // If the player has won, return true
+              if (hasWon) {
+                console.log(`game won by ${player}`);
+                isGameOver = true;
+                return;
+              }
             }
-            else {
-                isGameOver = false;
-            }
-        };
+            // If none of the combinations are satisfied, return false
+            return;
+          }
 
-//event listener to the reset button
+        //event listener to the reset button
         let resetButton = document.querySelector(".resetButton");
         
         resetButton.addEventListener("click", () => {
             reset();
         });
     
-//whenever reset is called move all the variables to its initial state
+        //whenever reset is called move all the variables to its initial state
         const reset = () => {
 
             updateCount = 1;
@@ -106,7 +127,7 @@ const gameBoard = (
 
         };
         
-        return {updateGameBoard, drawGameBoard, reset, gameStatus, playerX,playerO};
+        return {updateGameBoard, drawGameBoard, reset, checkForWin, playerX,playerO};
 
     }
 
